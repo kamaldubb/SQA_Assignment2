b9674fba65838222019e4b7138ea040673be13bc
Kamaljeet <kamaljeetkaurdub@gmail.com>
2021-02-26 13:47:03 -0500

    * .gitignore: complete solution for main class and rectangle solver   class
    * SQA_Assignment2.sln:
    * Program.cs:
    * TriangleSolver.cs:
    * SQA_Assignment2.csproj:

---
+++ SQA_Assignment2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace SQA_Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int j = 1;
+            while(j < 2)
+            { 
+                Console.WriteLine("Press 1 to Enter Triangle dimensions");
+                Console.WriteLine("Press 2 Exit");
+                string str = Console.ReadLine();
+                char firstChar = str[0];
+                bool isNumber = Char.IsDigit(firstChar);
+                if (j <= 0 || !isNumber)
+                {
+                    Console.WriteLine("Enter the valid number");
+                }
+                else
+                {
+                    Console.WriteLine(" ");
+                    int i = Convert.ToInt32(str);
+                    switch (i)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter the dimensions of triangle");
+                            int a = Convert.ToInt32(Console.ReadLine());
+                            int b = Convert.ToInt32(Console.ReadLine());
+                            int c = Convert.ToInt32(Console.ReadLine());
+                            TriangleSolver ts = new TriangleSolver();
+                            string str1 = ts.Analyze(a, b, c);
+                            Console.WriteLine(str1);
+                            break;
+
+                        case 2:
+                            Environment.Exit(0);
+                            break;
+
+
+                    }
+                }
+            }
+
+            
+        }
+    }
+}
---
+++ SQA_Assignment2/TriangleSolver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace SQA_Assignment2
+{
+    public class TriangleSolver
+    {
+
+       
+        public int a, b, c;
+        public string st;
+        public TriangleSolver()
+        {
+           
+        }
+        public string Analyze(int a1, int b1, int c1)
+        {
+            a = a1;
+            b = b1;
+            c = c1;
+            
+            if(Convert.ToBoolean(a) == Convert.ToBoolean(b) == Convert.ToBoolean(c))
+            {
+                st =  "Its is an equilateral triangle";
+                
+            }
+            else if(Convert.ToBoolean(a) == Convert.ToBoolean(b) != Convert.ToBoolean(c) || Convert.ToBoolean(a) != Convert.ToBoolean(b) == Convert.ToBoolean(c) || Convert.ToBoolean(a) != Convert.ToBoolean(c) == Convert.ToBoolean(b))
+            {
+                st = "Its is an isosceles triangle";
+            }
+            else if (Convert.ToBoolean(a) != Convert.ToBoolean(b) != Convert.ToBoolean(c))
+            {
+                st = "Its is an scalene triangle";
+
+            }
+            
+            return st;
+        }
+    }
+}
